28c3116e53ccf2b6f257a2cfa080f6bb26196864
Nabiel Alif <nabiel.alif01@gmail.com>
2023-05-18 16:17:07 +0700

    add all config folder

---
+++ nvim/lua/plugins/indentblankline.lua
@@ -0,0 +1,12 @@
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    -- char="",
+    show_current_context = true,
+    -- show_current_context_start = true,
+    -- show_end_of_line = true,
+    --
+    --
+    show_trailing_blankline_indent = false,
+    -- strict_tabs = true
+}
+
