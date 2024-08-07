9361e4c5f0c9d60e16a6012a1795acd193e40ff9
Nabiel Alif <nabiel.alif01@gmail.com>
2024-08-07 14:14:27 +0700

    feat: test

--- nvim/lua/plugins/packer.lua
+++ nvim/lua/plugins/packer.lua
@@ -19,12 +19,12 @@ return require('packer').startup(function(use)
 	use 'vim-test/vim-test'
 	use {
 		"nvim-neotest/neotest",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
-			"haydenmeade/neotest-jest",
-		}
+		 requires = {
+				"nvim-lua/plenary.nvim",
+				"nvim-treesitter/nvim-treesitter",
+				"antoinemadec/FixCursorHold.nvim",
+				"haydenmeade/neotest-jest",
+			}
 	}
 	use {
 		'nvim-tree/nvim-tree.lua',
