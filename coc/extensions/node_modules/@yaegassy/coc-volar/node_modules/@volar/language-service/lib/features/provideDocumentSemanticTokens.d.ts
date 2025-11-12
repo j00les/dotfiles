2c44028bd01b85cb03481c0fe11ad6bc6079e52a
Nabiel Alif <nabiel.alif01@gmail.com>
2025-11-13 04:57:39 +0700

    update

--- coc/extensions/node_modules/@yaegassy/coc-volar/node_modules/@volar/language-service/lib/features/provideDocumentSemanticTokens.d.ts
+++
@@ -1,4 +0,0 @@
-import type * as vscode from 'vscode-languageserver-protocol';
-import { URI } from 'vscode-uri';
-import type { LanguageServiceContext } from '../types';
-export declare function register(context: LanguageServiceContext): (uri: URI, range: vscode.Range | undefined, legend: vscode.SemanticTokensLegend, _reportProgress?: (tokens: vscode.SemanticTokens) => void, token?: vscode.CancellationToken) => Promise<vscode.SemanticTokens | undefined>;
--- coc/extensions/node_modules/@yaegassy/coc-volar/node_modules/@vue/language-server/bin/vue-language-server.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-// @ts-check
-if (process.argv.includes('--version')) {
-	console.log(require('../package.json').version);
-}
-else {
-	require('../node.js');
-}
